97f7e3b6fca4d533cabbafd472c85dbf2a36dfca
elmergustavo <elmergustavo79@gmail.com>
2022-04-03 00:47:34 -0600

    Manejando errores de respuestas

--- src/main/java/url/api/restaurantapi/controllers/ClienteRestCotroller.java
+++ src/main/java/url/api/restaurantapi/controllers/ClienteRestCotroller.java
@@ -1,12 +1,16 @@
 package url.api.restaurantapi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import url.api.restaurantapi.models.entity.Cliente;
 import url.api.restaurantapi.models.services.IClienteService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -23,8 +27,25 @@ public class ClienteRestCotroller {
 
     @GetMapping("/clientes/{id}")
     //@ResponseStatus(HttpStatus.OK)
-    public Cliente show(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", e.getMessage().concat(": ".concat(e.getMostSpecificCause().getMessage())));
+            return new ResponseEntity<Map<String, Object>>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: ".concat(id.toString().
+                    concat(" no existe en la base de datos!")));
+            return new ResponseEntity<Map<String, Object>>(response,HttpStatus.NOT_FOUND);
+
+        }
+        return new ResponseEntity<Cliente>(cliente,HttpStatus.OK);
     }
     
     @PostMapping("/clientes")
